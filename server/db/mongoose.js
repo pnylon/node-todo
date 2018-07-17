9ee44d27275436f4c3012e9e8c45daa2b2c544b1
Pierce Nylon <piercenylon@gmail.com>
2018-07-17 08:29:52 -0700

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,28 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, (error, db) => {
+//     if (error) {
+//         return console.log('Unable to connect to MongoDB sever');
+//     }
+//     console.log('Connected to mongoDB server');
+//     db.close();
+// }, );
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true
+}).then((db) => {
+    console.log('MongoDB TodoApp is connected')
+}).catch((error) => {
+    console.log(error)
+})
+
+// let connection = mongoose.connect('mongodb://localhost:27017/TodoApp', {
+//     useMongoClient: true
+// }).then(db => {
+//     console.log('MongoDB is connected')
+// }).catch(error => {
+//     console.log(error)
+// })
 
 module.exports = {mongoose};
